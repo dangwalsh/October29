32d0c16fbbb693795e6e51d6258e446592a89ca9
Daniel Walsh <dangwalsh@gmail.com>
2012-10-28 08:44:04 -0400

    Initial Commit

---
+++ October29/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  October29
+//
+//  Created by Daniel Walsh on 10/28/12.
+//  Copyright (c) 2012 Daniel Walsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
